22e492183e0ed223ce6c1bb75e74f9b4441ffd4a
Gavin Bagus <gavinbagus123@gmail.com>
2021-09-14 15:56:11 +0700

    initial commit, menambah fitur double jump

---
+++ Assets/Scripts/UIScoreController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScoreController : MonoBehaviour
+{
+    [Header("UI")]
+    public Text score;
+    public Text highScore;
+
+    [Header("Score")]
+    public ScoreController scoreController;
+
+    private void Update()
+    {
+        // score
+        score.text = scoreController.GetCurrentScore().ToString();
+        // high score
+        highScore.text = ScoreData.highScore.ToString();
+    }
+}
